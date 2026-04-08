d2e34fa95cd93c73404e53ecab9fb2d3d0e58b26
Sean P. Carapella <scarapella@amadeus.com>
2026-04-08 18:14:49 -0400

    revert defaulting of specific layers as it had weird side effects

--- js/index.js
+++ js/index.js
@@ -425,6 +425,8 @@
         router.setOptions(nogos.getOptions());
         router.setOptions(routingOptions.getOptions());
 
+        //TODO: for now we always load the default default active layers. we shoudl allow users to set preference
+        //layersControl.loadDefaultActiveLayers();
         // (check before hash plugin init)
         if (!location.hash) {
             profile.update(routingOptions.getOptions());
@@ -432,8 +434,6 @@
             // restore active layers from local storage when called without hash
             layersControl.loadActiveLayers();
         }
-        //TODO: for now we always load the default default active layers. we shoudl allow users to set preference
-        layersControl.loadDefaultActiveLayers();
 
         var onHashChangeCb = function (url) {
             var url2params = function (s = '') {
